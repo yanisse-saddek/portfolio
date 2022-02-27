906b01b1cae0e49d06155b8e34f0f40944a54187
yanisse-saddek <yanisse321@gmail.com>
2022-02-27 11:26:00 +0100

    add animations + contact section

--- js/script.js
+++ js/script.js
@@ -1,29 +1,70 @@
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if(scroll >=600){
-        $('.navbar').addClass('load-nav')
-        $('.navbar').removeClass('unload-nav')
-        $('.navbar').addClass('nav-fixed')
-        $('.navbar').removeClass('nav-absolute')
+            $(window).scroll(function() {
+            var scroll = window.scrollY
+            if(scroll < 500) {
+                $('.navbar').removeClass('nav-fixed')
+                $('.navbar').addClass('nav-absolute')
+                $('.navbar img').css({height: '100px'})
+            
+            } else {
+                 $('.navbar').removeClass('nav-absolute')
+                 $('.navbar').addClass('nav-fixed')
+                 $('.navbar img').css({height: '50px'})
+                }
+        });
+    
+
+        var $win = $(window);
+        var $project = $('.project');
+        $project.css('display', "none")
+        $win.on('scroll', function () {
+            var scrollTop = $win.scrollTop();
+            $project.each(function () {
+
+                var $self = $(this);
+                var prev=$self.offset();
+                if ((scrollTop - prev.top) > 1800) {
+                  $self.addClass('load')
+                  $self.css('display', 'flex')
+                  console.log(scrollTop - prev.top)
+                }
+        
+            }, 1000);
+        
+        }).scroll();
 
 
-        $('.navbar img').css({
-            height: '50px',
-        })
-    }else {
-        $('.navbar').removeClass('load-nav')
-        $('.navbar').addClass('unload-nav')
-        $('.navbar').removeClass('nav-fixed')
-        $('.navbar').addClass('nav-absolute')
+        var $win = $(window);
+        var $element = $('.show-element');
+        $element.css('display', "none")
+        $win.on('scroll', function () {
+            var scrollTop = $win.scrollTop();
+            $element.each(function () {
+                var $self = $(this);
+                var prev=$self.offset();
+                if ((scrollTop - prev.top) > 112) {
+                  $self.addClass('load')
+                  $self.css('display', 'flex')
+                }
+        
+            });
+        
+        }).scroll();
 
-    }
-})
+        
+        var $svgList = $('.show-svg');
+        $svgList.css('display', "none")
+        $win.on('scroll', function () {
+            var scrollTop = $win.scrollTop();
+            $svgList.each(function () {
+                var $self = $(this);
+                var prev=$self.offset();
+                if ((scrollTop - prev.top) > 2400) {
+                  $self.addClass('load')
+                  $self.css('display', 'flex')
+                  console.log(scrollTop - prev.top)
+                }
+        
+            });
+        
+        }).scroll();
 
-function greet() {
-    console.log('caca')
-    $('.navbar').addClass('nav-absolute')
-
-    $('.navbar img').css({
-        height: '100px'})
-    }
-   
