cb20937ecdb3cbd547b7e1072279d034d2360bb0
yanisse-saddek <yanisse321@gmail.com>
2022-04-16 12:46:55 +0200

    update

--- index.php
+++ index.php
@@ -152,7 +152,7 @@ if ($result = mysqli_query($conn, "SELECT * FROM portfolio")) {
                     <path
                     d="M256 352c-16.53 0-33.06-5.422-47.16-16.41L0 173.2V400C0 426.5 21.49 448 48 448h416c26.51 0 48-21.49 48-48V173.2l-208.8 162.5C289.1 346.6 272.5 352 256 352zM16.29 145.3l212.2 165.1c16.19 12.6 38.87 12.6 55.06 0l212.2-165.1C505.1 137.3 512 125 512 112C512 85.49 490.5 64 464 64h-416C21.49 64 0 85.49 0 112C0 125 6.01 137.3 16.29 145.3z" />
                 </svg></a>
-                <p class="mail">yanisse.sdk@gmail.com</p>
+                <p onClick="copy('mail')" id="mail" class="mail">yanisse.sdk@gmail.com</p>
             </div>
         </div>
         </div>
--- js/script.js
+++ js/script.js
@@ -112,3 +112,13 @@ $('.hamburger').click(() => {
 
 
 
+$('.particles-js-canvas-el').addClass('loadleft')
+function copy(elementId){
+    const temp = document.createElement("input");
+    const text = document.getElementById(elementId).innerHTML;
+    temp.setAttribute("value", text);
+    document.body.appendChild(temp);
+    temp.select();
+    document.execCommand("copy");
+    document.body.removeChild(temp);
+  }
